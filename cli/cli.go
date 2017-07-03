edd66e245acc54c309d6769a8a5e55ba89de3b65
Karina Thompson <klthompsn8@gmail.com>
2017-07-03 17:09:23 +1000

    refactoring

--- cli/cli.go
+++ cli/cli.go
@@ -9,16 +9,11 @@ import (
 	"github.com/karina-thompson/toy-robot-go/robot"
 )
 
-var errInvalidCommand = errors.New("Invalid command, please try again")
-
-func invalidExtraInput(command, parameters string) bool {
-	return command != "place" && len(parameters) != 0
-}
-
 func Run() {
 	fmt.Println("Toy Robot - please enter a command:")
 	r := robot.ToyRobot{}
 	acceptCommands := true
+	errInvalidCommand := errors.New("Invalid command, please try again")
 
 	for acceptCommands {
 		var command, parameters string
@@ -30,12 +25,8 @@ func Run() {
 		case invalidExtraInput(command, parameters):
 			err = errInvalidCommand
 			break
-		case command == "place" && len(parameters) != 0:
+		case validPlaceCommand(command, parameters):
 			position := strings.Split(parameters, ",")
-			if len(position) != 3 {
-				err = robot.ErrInvalidPosition
-				break
-			}
 			xPos, errX := strconv.Atoi(position[0])
 			yPos, errY := strconv.Atoi(position[1])
 			if errX != nil || errY != nil {
@@ -45,10 +36,8 @@ func Run() {
 			err = r.Place(xPos, yPos, position[2])
 		case command == "report":
 			err = r.Report()
-		case command == "left":
-			err = r.Turn("left")
-		case command == "right":
-			err = r.Turn("right")
+		case command == "left" || command == "right":
+			err = r.Turn(command)
 		case command == "move":
 			err = r.Move()
 		case command == "exit":
@@ -60,4 +49,12 @@ func Run() {
 			fmt.Println(err)
 		}
 	}
+}
+
+func invalidExtraInput(command, parameters string) bool {
+	return command != "place" && len(parameters) != 0
+}
+
+func validPlaceCommand(command, parameters string) bool {
+	return command == "place" && len(parameters) != 0 && len(strings.Split(parameters, ",")) == 3
 }
