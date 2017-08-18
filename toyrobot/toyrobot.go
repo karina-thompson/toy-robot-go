ba101f678ce641363d71141db69b7fb5709420b3
Karina Thompson <klthompsn8@gmail.com>
2017-08-18 15:33:26 +1000

    wip

--- toyrobot/toyrobot.go
+++ toyrobot/toyrobot.go
@@ -21,54 +21,40 @@ func Run(inputFile string) {
 	r := simulator.Robot{}
 	if inputFile == "" {
 		fmt.Println("Toy Robot - please enter a command:")
-		for {
-			command, parameters := getCliCommand()
+		stop := false
+		for !stop {
+			command := getCliCommand()
 			if command == exit {
+				stop = true
 				return
 			}
-			err := processCommand(&r, command, parameters)
-			if err != nil {
+			if err := processCommand(&r, command); err != nil {
 				fmt.Println(err)
 			}
 		}
 	}
 	data, err := ioutil.ReadFile(inputFile)
-	if err == nil {
-		commands := strings.Split(string(data), "\n")
-		for _, c := range commands {
-			command, parameters := parseInput(c)
-			err = processCommand(&r, command, parameters)
-			if err != nil {
-				fmt.Println(err)
-			}
+	if err != nil {
+		return
+	}
+	commands := strings.Split(string(data), "\n")
+	for _, c := range commands {
+		if err = processCommand(&r, c); err != nil {
+			fmt.Println(err)
 		}
 	}
 }
 
-func parseInput(input string) (string, string) {
-	splitInput := strings.Split(input, " ")
-	var parameters string
-	command := splitInput[0]
-	if len(splitInput) > 1 {
-		parameters = splitInput[1]
-	}
-	return command, parameters
-}
-
-func processCommand(r *simulator.Robot, command, parameters string) error {
+func processCommand(r *simulator.Robot, command string) error {
 	errInvalidCommand := errors.New("Invalid command, please try again")
 	command = strings.ToLower(command)
 	switch {
-	case invalidExtraInput(command, parameters):
-		return errInvalidCommand
-	case validPlaceCommand(command, parameters):
-		position := strings.Split(parameters, ",")
-		xPos, errX := strconv.Atoi(position[0])
-		yPos, errY := strconv.Atoi(position[1])
-		if errX != nil || errY != nil {
-			return simulator.ErrInvalidPosition
+	case validPlaceCommand(strings.Split(command, " ")):
+		xPos, yPos, direction, err := placeParameters(strings.Split(command, " "))
+		if err != nil {
+			return err
 		}
-		return r.Place(xPos, yPos, strings.ToLower(position[2]))
+		return r.Place(xPos, yPos, direction)
 	case command == report:
 		return r.Report()
 	case command == simulator.Left || command == simulator.Right:
@@ -80,15 +66,31 @@ func processCommand(r *simulator.Robot, command, parameters string) error {
 	}
 }
 
-func getCliCommand() (command, parameters string) {
-	fmt.Scanln(&command, &parameters)
-	return
+func getCliCommand() string {
+	var command, position string
+	fmt.Scanln(&command, &position)
+	if position == "" {
+		return command
+	}
+	return fmt.Sprintf("%v %v", command, position)
 }
 
-func invalidExtraInput(command, parameters string) bool {
-	return command != place && len(parameters) != 0
+func validPlaceCommand(command []string) bool {
+	if command[0] == place && len(command) == 2 {
+		if len(strings.Split(command[1], ",")) == 3 {
+			return true
+		}
+	}
+	return false
 }
 
-func validPlaceCommand(command, parameters string) bool {
-	return command == place && len(parameters) != 0 && len(strings.Split(parameters, ",")) == 3
+func placeParameters(command []string) (int, int, string, error) {
+	var err error
+	position := strings.Split(command[1], ",")
+	xPos, errX := strconv.Atoi(position[0])
+	yPos, errY := strconv.Atoi(position[1])
+	if errX != nil || errY != nil {
+		err = simulator.ErrInvalidPosition
+	}
+	return xPos, yPos, position[2], err
 }
