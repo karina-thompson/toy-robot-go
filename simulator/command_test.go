59417adf6f99d103af6d09acb604206d9addcb9c
Karina Thompson <klthompsn8@gmail.com>
2017-09-18 10:26:53 +1000

    Make command tests clearer

--- simulator/command_test.go
+++ simulator/command_test.go
@@ -8,26 +8,28 @@ import (
 
 func TestProcessCommand(t *testing.T) {
 	testData := []struct {
-		robot      *Robot
-		command    string
-		xPos, yPos int
-		facing     string
-		err        error
+		name                   string
+		robot                  *Robot
+		command                string
+		xPosResult, yPosResult int
+		facingResult           string
+		err                    error
 	}{
-		{&Robot{0, 0, north, true}, "move", 0, 1, north, nil},
-		{&Robot{1, 3, south, true}, "left", 1, 3, east, nil},
-		{&Robot{4, 4, west, true}, "right", 4, 4, north, nil},
-		{&Robot{}, "place 3,3,east", 3, 3, east, nil},
-		{&Robot{}, "place 3,east", 0, 0, "", errInvalidCommand},
-		{&Robot{}, "place", 0, 0, "", errInvalidCommand},
-		{&Robot{}, "place x,x,x", 0, 0, "", errInvalidPosition},
-		{&Robot{2, 0, north, true}, "report", 2, 0, north, nil},
+		{"Valid move command", &Robot{0, 0, north, true}, "move", 0, 1, north, nil},
+		{"Valid left command", &Robot{1, 3, south, true}, "left", 1, 3, east, nil},
+		{"Valid right command", &Robot{4, 4, west, true}, "right", 4, 4, north, nil},
+		{"Valid place command", &Robot{}, "place 3,3,east", 3, 3, east, nil},
+		{"Valid report command", &Robot{2, 0, north, true}, "report", 2, 0, north, nil},
+		{"Invalid place command with no coordinates", &Robot{}, "place", 0, 0, "", errInvalidCommand},
+		{"Invalid place coordinates", &Robot{}, "place 3,east", 0, 0, "", errInvalidCommand},
+		{"Invalid place coordinates", &Robot{}, "place x,x,x", 0, 0, "", errInvalidPosition},
 	}
 	for _, testCase := range testData {
+		assert := assert.New(t)
 		err := ProcessCommand(testCase.robot, testCase.command)
-		assert.Equal(t, testCase.robot.xPos, testCase.xPos)
-		assert.Equal(t, testCase.robot.yPos, testCase.yPos)
-		assert.Equal(t, testCase.robot.facing, testCase.facing)
-		assert.Equal(t, err, testCase.err)
+		assert.Equal(testCase.robot.xPos, testCase.xPosResult, testCase.name)
+		assert.Equal(testCase.robot.yPos, testCase.yPosResult, testCase.name)
+		assert.Equal(testCase.robot.facing, testCase.facingResult, testCase.name)
+		assert.Equal(err, testCase.err, testCase.name)
 	}
 }
